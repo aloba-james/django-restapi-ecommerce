95080aa9936668701817b8370ff5e52e6792046e
Aloba James <alobajames1@gmail.com>
2025-12-30 14:24:13 +0100

    added: serializers and subclasses

--- api/serializers.py
+++ api/serializers.py
@@ -20,15 +20,29 @@ class ProductSerializer(serializers.ModelSerializer):
         return value
     
 class OrderitemSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name')    
+    product_price = serializers.DecimalField(
+        max_digits=10,
+        decimal_places=2,
+        source='product.price')   
+
     class Meta:
         model = OrderItem
         fields = (
-            'product',
-            'quantity'
+            'product_name',
+            'product_price',
+            'quantity',
+            'item_subtotal'
         )   
 
 class OrderSerializer(serializers.ModelSerializer):
     items = OrderitemSerializer(many=True, read_only=True)
+    total_price = serializers.SerializerMethodField(method_name='total')
+
+    def total(self, obj):
+        order_items = obj.items.all()
+        return sum(order_item.item_subtotal for order_item in order_items)
+
     class Meta:
         model = Order
         fields = (
@@ -36,7 +50,14 @@ class OrderSerializer(serializers.ModelSerializer):
             'user',
             'created_at',
             'status',
-            'items'
+            'items',
+            'total_price'
         )
         
+
+class ProductInfoSerializer(serializers.Serializer):
+    products = ProductSerializer(many=True)
+    count = serializers.IntegerField()
+    max_price = serializers.FloatField()
+
 
